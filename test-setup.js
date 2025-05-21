399d9ffa158dd9833c8e4f4a3da358489d33842b
Xvezda <xvezda@naver.com>
2025-05-22 08:07:45 +0900

    Misc changes

--- test-setup.js
+++ test-setup.js
@@ -1,6 +1,7 @@
 const test = require('node:test');
 const { RuleTester } = require('@typescript-eslint/rule-tester');
 
+// https://typescript-eslint.io/packages/rule-tester/#nodejs-nodetest
 RuleTester.afterAll = test.after;
 RuleTester.describe = test.describe;
 RuleTester.it = test.it;
