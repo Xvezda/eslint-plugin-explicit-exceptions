818839f179486b557f7f78f82f992185beb51b39
Xvezda <xvezda@naver.com>
2025-05-22 07:59:54 +0900

    Configuration types and plugin metadata

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,17 @@
 "use strict";
 
+const fs = require('fs');
+const path = require('path');
+const packageJson = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf8'),
+);
+
+// @ts-expect-error
 module.exports = /** @type {import('eslint').ESLint.Plugin} */({
-  meta: {},
+  meta: {
+    name: packageJson.name,
+    version: packageJson.version,
+  },
   rules: {
     'exception-documentation': require('./rules/exception-documentation'),
     'no-implicit-propagation': require('./rules/no-implicit-propagation'),
