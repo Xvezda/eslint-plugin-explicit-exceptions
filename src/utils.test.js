6034a354a71a39ecc6863aa714ba21a2edba1a18
Xvezda <xvezda@naver.com>
2025-06-01 17:33:06 +0900

    Add tests

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-enum-comparison */
 const { test, describe } = require('node:test');
 const path = require('node:path');
 
@@ -10,8 +11,16 @@ const {
   TypeMap,
   getFirst,
   getLast,
-  hasThrowsTag,
+  getNodeID,
   getNodeIndent,
+  hasThrowsTag,
+  hasJSDocThrowsTag,
+  typeStringsToUnionString,
+  typesToUnionString,
+  findClosest,
+  findParent,
+  getCallee,
+  getCalleeDeclaration,
 } = require('./utils');
 
 /**
@@ -37,8 +46,10 @@ function parseCode(code) {
     services: /** @type {import('@typescript-eslint/utils').ParserServicesWithTypeInformation} */(
       parsed.services
     ),
+    // @ts-expect-error - incompatible
     sourceCode: new TSESLint.SourceCode({
       ...parsed,
+      parserServices: parsed.services,
       text: code,
     }),
   };
@@ -60,7 +71,7 @@ function foo() {
           ids.set(node.name, node);
         },
       },
-    });
+    }, true);
 
     const foo = ids.get('foo');
     const a = ids.get('a');
@@ -93,6 +104,47 @@ function foo() {
     t.assert.equal(getLast(['foo', 'bar']), 'bar');
   });
 
+  test('getNodeIndent', (t) => {
+    const { ast, sourceCode } = parseCode(`
+function foo() {
+  const bar: string = 'baz';
+}
+    `);
+
+    simpleTraverse(ast, {
+      visitors: {
+        /** @param {import('@typescript-eslint/typescript-estree').TSESTree.Identifier} node */
+        Identifier(node) {
+          if (node.name === 'bar') {
+            const indent = getNodeIndent(sourceCode, node);
+            t.assert.equal(indent.length, 2);
+          }
+        },
+      },
+    }, true);
+  });
+
+  test('getNodeID', (t) => {
+    const { ast } = parseCode(`
+function foo() {}
+function foo() {
+  const foo = 42;
+  function foo() {}
+}
+    `);
+
+    const ids = new Set();
+    simpleTraverse(ast, {
+      visitors: {
+        Identifier(node) {
+          ids.add(getNodeID(node));
+        }
+      },
+    }, true);
+
+    t.assert.equal(ids.size, 4);
+  });
+
   test('hasThrowsTag', (t) => {
     const { ast, sourceCode } = parseCode(`
 function foo() {
@@ -123,24 +175,18 @@ function bar() {
           functionNodes.push(node);
         },
       },
-    });
+    }, true);
 
     t.assert.equal(functionNodes.length, 2);
 
-    const foo = functionNodes.find((node) =>
-      node.type === AST_NODE_TYPES.FunctionDeclaration &&
-      node.id.name === 'foo'
-    );
+    const foo = functionNodes.find((node) => node.id.name === 'foo');
 
     t.assert.equal(
       hasThrowsTag(commentsToString(sourceCode.getCommentsBefore(foo))),
       false,
     );
     
-    const bar = functionNodes.find((node) =>
-      node.type === AST_NODE_TYPES.FunctionDeclaration &&
-      node.id.name === 'bar'
-    );
+    const bar = functionNodes.find((node) => node.id.name === 'bar');
 
     t.assert.equal(
       hasThrowsTag(commentsToString(sourceCode.getCommentsBefore(bar))),
@@ -148,23 +194,269 @@ function bar() {
     );
   });
 
-  test('getNodeIndent', (t) => {
+  test('hasJSDocThrowsTag', (t) => {
     const { ast, sourceCode } = parseCode(`
 function foo() {
-  const bar: string = 'baz';
+  throw new Error();
+}
+
+/**
+ * @throws {Error}
+ */
+function bar() {
+  throw new Error();
 }
     `);
 
+    /** @type {import('@typescript-eslint/utils').TSESTree.FunctionDeclaration[]} */
+    const functionNodes = [];
     simpleTraverse(ast, {
       visitors: {
-        /** @param {import('@typescript-eslint/typescript-estree').TSESTree.Identifier} node */
+        /** @param {import('@typescript-eslint/utils').TSESTree.FunctionDeclaration} node */
+        FunctionDeclaration(node) {
+          functionNodes.push(node);
+        },
+      },
+    }, true);
+
+    const foo = functionNodes.find((node) =>
+      node.id.name === 'foo'
+    );
+
+    t.assert.equal(hasJSDocThrowsTag(sourceCode, foo), false);
+    
+    const bar = functionNodes.find((node) => node.id.name === 'bar');
+
+    t.assert.equal(hasJSDocThrowsTag(sourceCode, bar), true);
+  });
+
+  test('typeStringsToUnionString', (t) => {
+    t.assert.equal(typeStringsToUnionString(['string']), 'string');
+    t.assert.equal(typeStringsToUnionString(['string', 'number']), 'string | number');
+  });
+
+  test('typesToUnionString', (t) => {
+    const { ast, services } = parseCode(`
+const a: number = 42;
+const b: string = 'foo';
+const c: string = 'bar';
+const d: number = 123;
+const e: string = 'baz';
+const f: number = 456;
+    `);
+
+    const checker = services.program.getTypeChecker();
+
+    const types = [];
+    simpleTraverse(ast, {
+      visitors: {
         Identifier(node) {
-          if (node.name === 'bar') {
-            const indent = getNodeIndent(sourceCode, node);
-            t.assert.equal(indent.length, 2);
+          types.push(services.getTypeAtLocation(node));
+        },
+      },
+    }, true);
+
+    t.assert.equal(
+      typesToUnionString(checker, types),
+      'number | string'
+    );
+  });
+
+  test('findClosest', (t) => {
+    const { ast } = parseCode(`
+function foo() {
+  function bar() {
+  }
+}
+    `);
+
+    /** @type {import('@typescript-eslint/typescript-estree').TSESTree.Identifier | null} */
+    let found = null;
+    simpleTraverse(ast, {
+      visitors: {
+        /** @param {import('@typescript-eslint/typescript-estree').TSESTree.FunctionDeclaration} node */
+        FunctionDeclaration(node) {
+          if (node.id.name === 'bar') {
+            found = node;
           }
         },
       },
-    });
+    }, true);
+
+    const closest = findClosest(
+      found, 
+      (n) => n.type === AST_NODE_TYPES.FunctionDeclaration
+    );
+
+    t.assert.ok(
+      closest?.type === AST_NODE_TYPES.FunctionDeclaration &&
+      closest.id.name === 'bar'
+    );
+  });
+
+  test('findParent', (t) => {
+    const { ast } = parseCode(`
+function foo() {
+  function bar() {
+  }
+}
+    `);
+
+    /** @type {import('@typescript-eslint/typescript-estree').TSESTree.Identifier | null} */
+    let found = null;
+    simpleTraverse(ast, {
+      visitors: {
+        /** @param {import('@typescript-eslint/typescript-estree').TSESTree.FunctionDeclaration} node */
+        FunctionDeclaration(node) {
+          if (node.id.name === 'bar') {
+            found = node;
+          }
+        },
+      },
+    }, true);
+
+    const closest = findParent(
+      found, 
+      (n) => n.type === AST_NODE_TYPES.FunctionDeclaration
+    );
+
+    t.assert.ok(
+      closest?.type === AST_NODE_TYPES.FunctionDeclaration &&
+      closest.id.name === 'foo'
+    );
+  });
+
+  test('getCallee', (t) => {
+    const { ast } = parseCode(`
+function foo() {}
+
+const obj = {
+  get bar() {},
+  set baz(value) {},
+};
+
+foo();
+const qux = obj.bar;
+obj.baz = 42;
+    `);
+
+    /** @type {Map<string, import('@typescript-eslint/typescript-estree').TSESTree.Node>} */
+    const map = new Map();
+    simpleTraverse(ast, {
+      visitors: {
+        [AST_NODE_TYPES.CallExpression](node) {
+          map.set(AST_NODE_TYPES.CallExpression, node);
+        },
+        [AST_NODE_TYPES.MemberExpression](node) {
+          if (node.parent?.type === AST_NODE_TYPES.AssignmentExpression) return;
+
+          map.set(AST_NODE_TYPES.MemberExpression, node);
+        },
+        [AST_NODE_TYPES.AssignmentExpression](node) {
+          map.set(AST_NODE_TYPES.AssignmentExpression, node);
+        },
+      },
+    }, true);
+
+    t.assert.equal(map.size, 3);
+
+    /** @type {import('@typescript-eslint/typescript-estree').TSESTree.CallExpression} */
+    const callExpression = map.get(AST_NODE_TYPES.CallExpression);
+    /** @type {import('@typescript-eslint/typescript-estree').TSESTree.MemberExpression} */
+    const memberExpression = map.get(AST_NODE_TYPES.MemberExpression);
+    /** @type {import('@typescript-eslint/typescript-estree').TSESTree.AssignmentExpression} */
+    const assignmentExpression = map.get(AST_NODE_TYPES.AssignmentExpression);
+
+    t.assert.ok(
+      getCallee(callExpression).type === AST_NODE_TYPES.Identifier &&
+      getCallee(callExpression).name === 'foo'
+    );
+
+    t.assert.ok(
+      getCallee(memberExpression).type === AST_NODE_TYPES.Identifier &&
+      getCallee(memberExpression).name === 'bar',
+      'accessing property returns getter and must be named "bar"'
+    );
+
+    t.assert.ok(
+      getCallee(assignmentExpression).type === AST_NODE_TYPES.MemberExpression &&
+      /** @type {import('@typescript-eslint/typescript-estree').TSESTree.MemberExpression} */
+      (getCallee(assignmentExpression)).property.type === AST_NODE_TYPES.Identifier &&
+      /** @type {import('@typescript-eslint/typescript-estree').TSESTree.MemberExpression} */
+      (getCallee(assignmentExpression)).property.name === 'baz',
+      'assignment to member expression returns setter must be named "baz"'
+    );
+  });
+
+  test('getCallee', (t) => {
+    const { ast, services, sourceCode } = parseCode(`
+// foo declaration
+function foo() {}
+
+const obj = {
+  // bar declaration
+  get bar() {},
+  // baz declaration
+  set baz(value) {},
+};
+
+foo();
+const qux = obj.bar;
+obj.baz = 42;
+    `);
+
+    /** @type {Map<string, import('@typescript-eslint/typescript-estree').TSESTree.Node>} */
+    const map = new Map();
+    simpleTraverse(ast, {
+      visitors: {
+        [AST_NODE_TYPES.CallExpression](node) {
+          map.set(AST_NODE_TYPES.CallExpression, node);
+        },
+        [AST_NODE_TYPES.MemberExpression](node) {
+          if (node.parent?.type === AST_NODE_TYPES.AssignmentExpression) return;
+
+          map.set(AST_NODE_TYPES.MemberExpression, node);
+        },
+        [AST_NODE_TYPES.AssignmentExpression](node) {
+          map.set(AST_NODE_TYPES.AssignmentExpression, node);
+        },
+      },
+    }, true);
+
+    t.assert.equal(map.size, 3);
+
+    /** @type {import('@typescript-eslint/typescript-estree').TSESTree.CallExpression} */
+    const callExpression = map.get(AST_NODE_TYPES.CallExpression);
+    /** @type {import('@typescript-eslint/typescript-estree').TSESTree.MemberExpression} */
+    const memberExpression = map.get(AST_NODE_TYPES.MemberExpression);
+    /** @type {import('@typescript-eslint/typescript-estree').TSESTree.AssignmentExpression} */
+    const assignmentExpression = map.get(AST_NODE_TYPES.AssignmentExpression);
+
+    t.assert.ok(
+      sourceCode
+        .getCommentsBefore(
+          services.tsNodeToESTreeNodeMap
+            .get(getCalleeDeclaration(services, callExpression))
+        )
+        .some(({ value }) => value.includes('foo declaration')),
+    );
+
+    t.assert.ok(
+      sourceCode
+        .getCommentsBefore(
+          services.tsNodeToESTreeNodeMap
+            .get(getCalleeDeclaration(services, memberExpression)),
+        )
+        .some(({ value }) => value.includes('bar declaration')),
+    );
+
+    t.assert.ok(
+      sourceCode
+        .getCommentsBefore(
+          services.tsNodeToESTreeNodeMap
+            .get(getCalleeDeclaration(services, assignmentExpression)),
+        )
+        .some(({ value }) => value.includes('baz declaration')),
+    );
   });
 });
