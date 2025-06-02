cbf17fdb23b08dddfc1f3a72cf457e8bd2eaf75c
Xvezda <xvezda@naver.com>
2025-06-03 01:44:13 +0900

    Deprecate `no-implicit-propagation` rule

--- src/plugin.js
+++ src/plugin.js
@@ -8,7 +8,6 @@ const plugin = {
   },
   rules: {
     'no-undocumented-throws': require('./rules/no-undocumented-throws'),
-    'no-implicit-propagation': require('./rules/no-implicit-propagation'),
     'no-unhandled-rejection': require('./rules/no-unhandled-rejection'),
     'check-throws-tag-type': require('./rules/check-throws-tag-type'),
   },
@@ -16,7 +15,6 @@ const plugin = {
 
 const rules = /** @type {const} */({
   'explicit-exceptions/no-undocumented-throws': 'error',
-  'explicit-exceptions/no-implicit-propagation': 'error',
   'explicit-exceptions/no-unhandled-rejection': 'error',
   'explicit-exceptions/check-throws-tag-type': 'error',
 });
